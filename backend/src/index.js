d859e36041c0f7e745787aa147299c77fee45bc5
Dima <admin@admins-MacBook-Pro.local>
2023-05-31 15:24:06 +0300

    Init backend

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(PORT);
