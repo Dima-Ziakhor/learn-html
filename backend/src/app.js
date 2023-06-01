c72f4a3d16dcde322c025fbc9f221275e99754f4
Dima <admin@admins-MacBook-Pro.local>
2023-06-01 23:13:08 +0300

    Try to fix prod

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-// import { themeRouter } from './routes/themeRouter.js';
+import { themeRouter } from './routes/themeRouter.js';
 // import { paragraphRouter } from './routes/paragraphRouter.js';
 
 const app = express();
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
-// app.use(themeRouter);
+app.use(themeRouter);
 // app.use(paragraphRouter);
 app.get('/', (req, res) => {
   res.send('Hello!');
